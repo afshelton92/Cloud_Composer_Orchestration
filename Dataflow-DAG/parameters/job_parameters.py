32000d9a8a5fabb01d96120301b133a8bd7f3a77
afshelton92 <31833882+afshelton92@users.noreply.github.com>
2020-03-30 09:03:36 -0400

    Update job_parameters.py

--- Dataflow-DAG/parameters/job_parameters.py
+++ Dataflow-DAG/parameters/job_parameters.py
@@ -1,10 +1,10 @@
 dataflow={
-    "project": "totemic-splicer-272114",
+    "project": "[project_id]",
     "job_name": 'my_job',
-    "temp_location": "gs://totemic-splicer-272114-df-test/temp",
-    "staging_location": "gs://totemic-splicer-272114-df-test/staging"
+    "temp_location": "[temp location for job storage]",
+    "staging_location": "[staging location for job storage]"
     }
 other_options={
-    'input': "gs://totemic-splicer-272114-df-test/Input_Files/tf_test.txt",
-    'output': "gs://totemic-splicer-272114-df-test/Output_Files/tf_test.txt"
+    'input': "[path_to_input_files]",
+    'output': "[path_to_output]"
     }
